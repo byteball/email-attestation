0ec90174f792562921abf2c7db5557e1195d30a7
Tarmo Annus <tarmo888@hotmail.com>
2018-05-11 22:23:45 +0300

    fix

--- modules/email_attestation.js
+++ modules/email_attestation.js
@@ -78,7 +78,7 @@ function postAndWriteAttestation(transaction_id, attestor_address, attestation_p
 						[unit, transaction_id],
 						() => {
 							db.query(
-								`SELECT lang FROM attestation_units JOIN attestations ON a.unit = attestation_units.attestation_unit JOIN users ON attestations.address = users.user_address WHERE transaction_id = ? LIMIT 1`,
+								`SELECT lang FROM attestation_units JOIN attestations ON attestations.unit = attestation_units.attestation_unit JOIN users ON attestations.address = users.user_address WHERE transaction_id = ? LIMIT 1`,
 								[transaction_id],
 								(users) => {
 									let device = require('byteballcore/device.js');
