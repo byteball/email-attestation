0c0236e1fe1c9e9a18aacca8d3e2f7185c6e716b
tony <tonyofbyteball@users.noreply.github.com>
2018-05-17 13:11:45 +0300

    add lang

--- db.sql
+++ db.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS users (
 	device_address CHAR(33) NOT NULL PRIMARY KEY,
 	user_address CHAR(32) NULL,
 	user_email VARCHAR(320) NULL,
+	lang CHAR(20) NOT NULL DEFAULT 'unknown',
 	creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	FOREIGN KEY (device_address) REFERENCES correspondent_devices(device_address)
 );
